6c36410a40c884ceb560104906815e6bef8bb327
Martin0280 <martinheinonen08@gmail.com>
2024-09-26 13:08:56 +0300

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,14 @@
+public class Java1 {
+    public static void main(String args[]) {
+        System.out.println("Hej jag heter");
+    String name = "Martin ";
+    String lastname = "Heinonen";
+        System.out.println(name + lastname);
+        int Num = 15;
+        System.out.println("Jag ar " + Num + " ar gammal");
+        double Height = 183.5;
+        System.out.println(Height + " cm");
+        boolean Isastudent = true; 
+        System.out.println(Isastudent);
+    }
+}
