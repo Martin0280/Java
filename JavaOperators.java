5caf6d6e639347c6348b0e8c148161e77055dffc
Martin0280 <martinheinonen08@gmail.com>
2024-09-26 12:13:40 +0300

    Add files via upload

---
+++ JavaOperators.java
@@ -0,0 +1,14 @@
+public class JavaOperators {
+
+    public static void main(String args[]) {
+
+    int sum = 100 + 200; 
+            System.out.println (sum);
+    int x = 5;
+    int y = 15;
+            System.out.println ("x + y = "+ (x + y));
+            System.out.println ("x - y = "+ (x - y));
+            System.out.println ("x * y = "+ x * y);
+            System.out.println ("x / y = "+ y / x);
+    }
+}
