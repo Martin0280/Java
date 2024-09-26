4161d88dae5ab70eb165c1696fd17f1776a5a783
Martin0280 <martinheinonen08@gmail.com>
2024-09-26 12:14:19 +0300

    Add files via upload

---
+++ JavaVariables.java
@@ -0,0 +1,26 @@
+
+
+
+public class JavaVariables {
+
+    
+    public static void main(String args[]) {
+        //Text variable
+        String name = "Johan";
+        String lastname = "Forssten";
+        System.out.print("Hej! ");
+        System.out.println(name + " " + lastname);
+        //int Variable
+        int myNum = 24;
+        System.out.println (myNum);
+        
+       
+        float floatNum = 5.99f;
+        char myLetter = 'D'; 
+        boolean myBool = true;       
+        System.out.println (floatNum);
+        System.out.println (myLetter);
+        System.out.println (myBool);
+        
+    }
+}
