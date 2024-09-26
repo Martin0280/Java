de0cd79a6ee1a0f857c2650afbbd5bfb0e7a4efa
Martin0280 <martinheinonen08@gmail.com>
2024-09-26 12:14:09 +0300

    Add files via upload

---
+++ Javasyntax.java
@@ -0,0 +1,16 @@
+
+
+public class Javasyntax {
+
+
+           public static void main (String args[]) {
+       System.out.println("Java is fun");// prints text
+       //System.out.println("SOul + tab shortcut");
+       /*
+       System.out.println(3);
+       System.out.println(30);
+       System.out.println(300);
+       */
+    }
+}           
+                 
