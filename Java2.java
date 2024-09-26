a0670854585d4fcb75d6141e3b06a5d928a21f33
Martin0280 <martinheinonen08@gmail.com>
2024-09-26 13:32:06 +0300

    Add files via upload

---
+++ Java2.java
@@ -0,0 +1,14 @@
+public class Java2 {
+
+    public static void main(String args[]) {
+        int Num1 = 5;
+        int Num2 = 35;
+        
+        System.out.println(Num1 + Num2);
+        System.out.println(Num2 - Num1);
+        System.out.println(Num1 * Num2);
+        System.out.println(Num1 / Num2);
+        
+        
+            }
+}
